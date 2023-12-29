f8e4a1ba8b927bc9156c6a58c4e2c1a474fa556f
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-12-29 14:24:59 +0200

    Add admin register

--- functionality/index.js
+++ functionality/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressSession = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
@@ -8,9 +9,17 @@ const userHome = require('./src/user/home');
 //admin
 const adminHome = require('./src/admin/home');
 const adminRegister = require('./src/admin/register');
+const { registerAdmin } = require('./src/admin/requests/register');
 
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(expressSession({
+    secret: 'secret cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: 'auto' }
+}));
 
 const hbs = exphbs.create({ extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views/layouts/') });
 app.engine('hbs', hbs.engine);
@@ -21,5 +30,10 @@ app.get('/', userHome);
 
 app.get('/admin', adminHome);
 app.get('/admin/register', adminRegister);
+
+app.post('/admin/register', async (req, res) => {
+    await registerAdmin(req, res);
+});
+
 
 app.listen(3000);
