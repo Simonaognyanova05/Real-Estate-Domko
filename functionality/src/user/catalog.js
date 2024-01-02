8458724e85a88a6ff198182bafbb7a50db646409
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-02 20:14:32 +0200

    Add get all request

--- functionality/src/user/catalog.js
+++ functionality/src/user/catalog.js
@@ -1,5 +1,7 @@
 const { getAll } = require('./requests/getAll');
 
+
+//
 module.exports = async (req, res) => {
     const data = await getAll(req, res);
     res.render('user/catalog', { title: 'Съзадане на администратор', user: req.session.user, property: data });
