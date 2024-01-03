261d7172640d3aa4f73ddce5b1bc7067d58c7cf2
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-03 12:44:01 +0200

    Add admin home page

--- functionality/index.js
+++ functionality/index.js
@@ -3,12 +3,16 @@ const expressSession = require('express-session');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
+//user
 const userHome = require('./src/user/home');
 const userAbout = require('./src/user/about');
 const userRent = require('./src/user/rent');
 const userSales = require('./src/user/sales');
 const userGallery = require('./src/user/gallery');
 const userContacts = require('./src/user/contacts');
+
+//admin
+const adminHome = require('./src/admin/home');
 
 const app = express();
 const hbs = exphbs.create({ extname: 'hbs', defaultLayout: 'main', layoutsDir: path.join(__dirname, 'views/layouts/') });
@@ -25,11 +29,15 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.use('/content', express.static('static'));
 
+//user
 app.get('/', userHome);
 app.get('/about', userAbout);
 app.get('/rent', userRent);
 app.get('/sales', userSales);
 app.get('/gallery', userGallery);
 app.get('/contacts', userContacts);
+
+//admin
+app.get('/admin', adminHome);
 
 app.listen(3000);
