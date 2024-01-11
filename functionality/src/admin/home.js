7cf817d83487d1c2cc3f6dd1f116b9e857102fc6
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-11 08:09:07 +0200

    Add checking if has admin or not

--- functionality/src/admin/home.js
+++ functionality/src/admin/home.js
@@ -1,3 +1,8 @@
 module.exports = (req, res) => {
-    res.render('admin/home', { title: 'Начало | Администратор', layout: 'mainAdmin', admin: req.session.admin});
+    if (req.session.admin) {
+        res.render('admin/home', { title: 'Начало | Администратор', layout: 'mainAdmin', admin: req.session.admin });
+    } else {
+        res.render('admin/login', { title: "Вход за администратор | Администратор", layout: 'mainAdmin'});
+
+    }
 }
