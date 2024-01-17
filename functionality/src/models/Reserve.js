66119b0d91ea8ab1f5df24e38cb921b88aa9d16b
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-17 09:29:28 +0200

    Some changes

--- functionality/src/models/Reserve.js
+++ functionality/src/models/Reserve.js
@@ -4,11 +4,11 @@ const reserveSchema = new mongoose.Schema({
     type: {type: String, required: true},
     location: {type: String, required: true},
     address: {type: String, required: true},
-    floors: {type: Number, required: true},
-    rooms: {type: Number, required: true},
-    squareMeters: {type: Number, required: true},
-    price: {type: Number, required: true},
-    priceForHour: {type: Number, required: true},
+    floors: {type: String, required: true},
+    rooms: {type: String, required: true},
+    squareMeters: {type: String, required: true},
+    price: {type: String, required: true},
+    priceForHour: {type: String, required: true},
     img1: {type: String, required: true},
     img2: {type: String, required: true},
     img3: {type: String, required: true},
@@ -17,7 +17,6 @@ const reserveSchema = new mongoose.Schema({
     names: {type: String, required: true},
     date: {type: String, required: true},
     time: {type: String, required: true},
-    ownerId: {type: String, required: true},
 });
 
 const Reserve = mongoose.model('Reserve', reserveSchema);
