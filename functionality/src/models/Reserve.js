ec9336153dd34544c1526cf7195eb3deb50a9018
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-19 13:02:44 +0200

    Add total price to ownReservation

--- functionality/src/models/Reserve.js
+++ functionality/src/models/Reserve.js
@@ -17,6 +17,7 @@ const reserveSchema = new mongoose.Schema({
     names: {type: String, required: true},
     date: {type: String, required: true},
     time: {type: String, required: true},
+    totalPrice: {type: Number, required: true},
     ownerId: {type: String, required: true}
 });
 
--- functionality/src/user/requests/reservation.js
+++ functionality/src/user/requests/reservation.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Reserve = require('../../models/Reserve');
 const { clearCart } = require('./clearCart');
+const getTotalPrice = require('../requests/getTotalPriceInBag');
 
 
 const dbUrl = 'mongodb+srv://domko:fSSNqd7zySE0aZdW@cluster0.soh1wrl.mongodb.net/';
@@ -14,9 +15,9 @@ async function reservation(req, res) {
     const { type, location, address, floors, rooms, squareMeters, price, priceForHour, img1, img2, img3, img4, img5, names, date, time } = req.body;
 
     try {
-        
+        const totalPrice = await getTotalPrice(req, res);
         const reserve = new Reserve({
-            type, location, address, floors, rooms, squareMeters, price, priceForHour, img1, img2, img3, img4, img5, names, date, time, ownerId: req.session.user._id
+            type, location, address, floors, rooms, squareMeters, price, priceForHour, img1, img2, img3, img4, img5, names, date, time, totalPrice ,ownerId: req.session.user._id
         })
         await reserve.save();
         await clearCart(req, res);
