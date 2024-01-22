735f00723c11ed6687d6bc28fc545f1b6d0121e5
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-22 09:20:05 +0200

    Add solving problems

--- functionality/src/user/galleryRents.js
+++ functionality/src/user/galleryRents.js
@@ -3,5 +3,5 @@ const { getRentData } = require("./requests/getRentData");
 
 module.exports = async (req, res) => {
     const rentData = await getRentData(req, res);
-    res.render('user/galleryRents', { title: "Галерия | Домко", rentData: rentData});
+    res.render('user/galleryRents', { title: "Галерия | Домко", rentData: rentData, user: req.session.user});
 }
