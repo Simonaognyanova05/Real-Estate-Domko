83af8346846df96265366661a5f7fddca8ca9c09
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-01-23 08:53:53 +0200

    Add filtering

--- functionality/src/user/filterRents.js
+++ functionality/src/user/filterRents.js
@@ -2,4 +2,5 @@ module.exports = async (req, res, rents) => {
     res.render('user/filterRents', {
         title: "Филтрирани данни | Домко",
         rents: rents,
+        user: req.session.user
 })}
