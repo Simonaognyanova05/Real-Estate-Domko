d4e955f68708438c398cdf431496163cc648d9ae
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-03-27 12:44:12 +0200

    Add checking if eastate is included in cart

--- functionality/src/user/requests/addToCart.js
+++ functionality/src/user/requests/addToCart.js
@@ -6,16 +6,55 @@ const connectionParams = {
     useUnifiedTopology: true,
     useNewUrlParser: true
 }
+
 async function addToCart(req, res) {
     await mongoose.connect(dbUrl, connectionParams);
 
     const { type, location, address, floors, rooms, squareMeters, price, priceForHour, img1, img2, img3, img4, img5 } = req.body;
 
     try {
+        const existingProduct = await Cart.findOne({
+            type: type.toString(),
+            location: location.toString(),
+            address: address.toString(),
+            floors: Number(floors),
+            rooms: Number(rooms),
+            squareMeters: Number(squareMeters),
+            price: Number(price),
+            priceForHour: Number(priceForHour),
+            img1: img1.toString(),
+            img2: img2.toString(),
+            img3: img3.toString(),
+            img4: img4.toString(),
+            img5: img5.toString(),
+            ownerId: req.session.user._id
+        });
+
+        if (existingProduct) {
+            res.send(`<script>
+            alert('Имотът вече е добавен в количката');
+            window.location.href = '/';
+            </script>`);
+            return;
+        }
+
         const cart = new Cart({
-            type: type.toString(), location: location.toString(), address: address.toString(), floors: Number(floors), rooms: Number(rooms), squareMeters: Number(squareMeters), price: Number(price), priceForHour: Number(priceForHour), img1: img1.toString(), 
-            img2: img2.toString(), img3: img3.toString(), img4: img4.toString(), img5: img5.toString(), ownerId: req.session.user._id
-        })
+            type: type.toString(),
+            location: location.toString(),
+            address: address.toString(),
+            floors: Number(floors),
+            rooms: Number(rooms),
+            squareMeters: Number(squareMeters),
+            price: Number(price),
+            priceForHour: Number(priceForHour),
+            img1: img1.toString(),
+            img2: img2.toString(),
+            img3: img3.toString(),
+            img4: img4.toString(),
+            img5: img5.toString(),
+            ownerId: req.session.user._id
+        });
+
         await cart.save();
         res.redirect('/');
     } catch (e) {
