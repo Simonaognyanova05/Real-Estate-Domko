a220a187490bad9e6a4552420ba12ef9d209c8cc
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-03-27 10:14:36 +0200

    Add dynamic favourites page

--- functionality/src/user/favourites.js
+++ functionality/src/user/favourites.js
@@ -1,11 +1,12 @@
-// const { getFavouritesData } = require('./requests/getFavouritesData');
+const { getFavouritesData } = require('./requests/getFavouritesData');
 
 module.exports = async (req, res) => {
-    // const favouritesData = await getFavouritesData(req, res);
+    const favouritesData = await getFavouritesData(req, res);
 
     res.render('user/favourites', {
         title: "Любими | Домко",
         user: req.session.user,
+        favouritesData: favouritesData
     });
 
 }
