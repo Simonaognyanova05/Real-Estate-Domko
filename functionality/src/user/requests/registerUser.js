83930b622fc04cc8ea1fa5ce25f93ad2affe7263
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-04-15 15:21:00 +0300

    Add one more checing if user exist

--- functionality/src/user/requests/registerUser.js
+++ functionality/src/user/requests/registerUser.js
@@ -9,48 +9,45 @@ const connectionParams = {
     useNewUrlParser: true
 }
 
-
 async function registerUser(req, res) {
     await mongoose.connect(dbUrl, connectionParams);
 
     const { username, password, email, ages, location, profileImg, interests, rePass } = req.body;
 
     if (password != rePass) {
-        res.send(`<script>
-        
-        alert('Паролите не съвпадат!');
-        window.location.href = '/register';
-        </script>`);
+        res.send(`<script>alert('Паролите не съвпадат!'); window.location.href = '/register';</script>`);
         return;
     }
     if (password.length < 6) {
-        res.send(`<script>
-        
-        alert('Паролата трябва да е минимум 6 символа!');
-        window.location.href = '/register';
-        </script>`);
+        res.send(`<script>alert('Паролата трябва да е минимум 6 символа!'); window.location.href = '/register';</script>`);
         return;
     }
     if (username.length < 5) {
-        res.send(`<script>
-        
-        alert('Потребителското име трябва да е минимум 5 символа!');
-        window.location.href = '/register';
-        </script>`);
+        res.send(`<script>alert('Потребителското име трябва да е минимум 5 символа!'); window.location.href = '/register';</script>`);
         return;
     }
 
-    const hashedPass = await bcrypt.hash(password, 10);
     try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            res.send(`<script>alert('Потребителското име вече съществува!'); window.location.href = '/register';</script>`);
+            return;
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
         const user = new User({
             username, email, hashedPass, ages, location, profileImg, interests
-        })
+        });
         await user.save();
         req.session.user = user;
         res.redirect('/');
     } catch (e) {
-        console.log(e);
+        console.error(e);
+        res.status(500).send('Грешка при регистрация на потребител.');
+    } finally {
+        mongoose.disconnect();
     }
 }
 
 module.exports = { registerUser };
+
